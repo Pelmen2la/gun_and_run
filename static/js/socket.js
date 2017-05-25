843fa459224782eb8efec2f9a7799425550948a1
Pelmen2la <pelmengi12>
2017-05-25 10:03:42 +0300

    Added split to socket address to avoid double port

--- static/js/socket.js
+++ static/js/socket.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 var socket;
 
 function init(handlers, callback) {
-    socket = io(window.location.origin + ':8100');
+    socket = io(window.location.origin.split(':')[0] + ':8100');
     socket.on('connect', function() {
         socket.on('roomData', function(data) {
             handlers.onRoomData(data);
